c37a017d87a7bfd0d38e5ecf8e02419d03c690b4
Tiny Taimi <OpNop@users.noreply.github.com>
2021-03-20 13:48:15 -0700

    Added User Guild component

---
+++ src/components/UserGuild.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="is-user-avatar">
+    <img :src="getEmblem" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserGuild',
+  props: {
+    guild: {
+      type: String,
+      default: null
+    },
+    rank: {
+      type: String,
+      default: "Almost Tiny"
+    }
+  },
+  computed: {
+    getEmblem () {
+      return `https://emblem.werdes.net/emblem/${this.guild}`
+      //`https://avatars.dicebear.com/v2/human/${name}.svg?options[mood][]=happy`
+    }
+  }
+}
+</script>
