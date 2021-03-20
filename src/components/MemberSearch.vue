bd187aefddd97b3ac6be2384f06e1ebccb94b9a5
Tiny Taimi <OpNop@users.noreply.github.com>
2021-03-20 13:47:40 -0700

    Added MemberSearch Component

---
+++ src/components/MemberSearch.vue
@@ -0,0 +1,72 @@
+<template>
+  <b-field>
+    <b-autocomplete
+      v-model="account"
+      :data="data"
+      placeholder="Search Members..."
+      field="account"
+      type="search"
+      icon="magnify"
+      size="is-medium"
+      :loading="isLoading"
+      @typing="getAsyncData"
+      @select="gotToUser"
+      expanded
+      keep-first
+      clearOnSelect
+    >
+    </b-autocomplete>
+  </b-field>
+</template>
+
+<script>
+import debounce from 'lodash.debounce'
+import axios from 'axios'
+
+export default {
+  name: 'MemberSearch',
+  data() {
+    return {
+      account: '',
+      data: [],
+      selected: null,
+      isLoading: false
+    }
+  },
+  methods: {
+    getAsyncData: debounce(function(account) {
+      if (!account.length) {
+        this.data = []
+        return
+      }
+      this.isLoading = true
+      axios
+        .get(
+          `https://api.tinyarmy.org/v1/guild/members/search?account=${account}`
+        )
+        .then(r => {
+          this.data = r.data.map(item => item)
+        })
+        .catch(error => {
+          this.data = []
+          this.$buefy.toast.open({
+            message: `Error: ${error.message}`,
+            type: 'is-danger'
+          })
+        })
+        .finally(() => {
+          this.isLoading = false
+        })
+    }, 500),
+
+    gotToUser(account) {
+      if (account) {
+        this.$router.push({
+          name: 'member.view',
+          params: { account: account.account }
+        })
+      }
+    }
+  }
+}
+</script>
