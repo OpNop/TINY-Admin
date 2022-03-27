6fe119fd90889c58c3402c4ef50b206b579162be
Tiny Taimi <OpNop@users.noreply.github.com>
2022-03-27 02:00:20 -0700

    Added Markdown logo and link to Member Message UI

--- src/views/MemberProfile.vue
+++ src/views/MemberProfile.vue
@@ -90,7 +90,18 @@
                 </span>
               </template>
               <div class="notification">
-                <b-field label="Message">
+                <b-field>
+                  <template #label>
+                    Message
+                    <a href="https://www.markdownguide.org/basic-syntax/" target="_blank" title="Markdown Guide">
+                      <svg style="width: 24px; height: 24px" viewBox="0 0 24 10">
+                        <path
+                          fill="currentColor"
+                          d="M20.56 18H3.44C2.65 18 2 17.37 2 16.59V7.41C2 6.63 2.65 6 3.44 6H20.56C21.35 6 22 6.63 22 7.41V16.59C22 17.37 21.35 18 20.56 18M6.81 15.19V11.53L8.73 13.88L10.65 11.53V15.19H12.58V8.81H10.65L8.73 11.16L6.81 8.81H4.89V15.19H6.81M19.69 12H17.77V8.81H15.85V12H13.92L16.81 15.28L19.69 12Z"
+                        />
+                      </svg>
+                    </a>
+                  </template>
                   <b-input
                     rows="3"
                     type="textarea"
