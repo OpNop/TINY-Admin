83c684b90951386023e59c39fce6163702c1c117
Tiny Taimi <OpNop@users.noreply.github.com>
2022-05-09 19:52:48 -0700

    Update WelcomeMessages.vue

--- src/views/WelcomeMessages.vue
+++ src/views/WelcomeMessages.vue
@@ -8,17 +8,14 @@
         <b-input customClass="is-family-code has-fixed-size" maxlength="511" rows="21" cols="53" type="textarea" readonly value="THE GOLDEN RULES OF TINY
 1) Only rep [TINY] while on an Asura.
 2) Be nice to EVERYONE! Even bookahs!
-3) No Politics or Religion in guild chat. 
+3) No Politics, Religion, Excessive Profanity, or Drama in guild chats.
 
-SCHEDULED GUILD EVENTS 
-Tiny Takeover Tuesday, Guild Missions, Raids, Lottery 
---- Full times &amp; details 
---- https://tinyarmy.org/#events
+GUILD RATING of E10+
+(Further rule details can be found on our websites and Discord)
 
-LINKS 
+LINKS
 Website  - https://tinyarmy.org
-Discord  - https://tinyarmy.org/discord
-Facebook - https://tinyarmy.org/facebook
+Discord  - https://discord.gg/tinyarmy
 
 Discord is the best place to keep updated
 with the guild and is HIGHLY recommended.">
