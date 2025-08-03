96c6fd945fdb102fae8c00e87a09359eba0cac22
Tiny Taimi <OpNop@users.noreply.github.com>
2025-08-03 16:03:38 -0700

    Display for 0 donations

--- src/components/GwGold.vue
+++ src/components/GwGold.vue
@@ -1,8 +1,13 @@
 <template>
   <span :class="{small: 'small'}">
-    <span v-if='gw2.gold'>{{gw2.gold.toLocaleString()}}<img src='https://render.guildwars2.com/file/090A980A96D39FD36FBB004903644C6DBEFB1FFB/156904.png'/> </span>
-    <span v-if='gw2.silver'>{{gw2.silver.toLocaleString()}}<img src='https://render.guildwars2.com/file/E5A2197D78ECE4AE0349C8B3710D033D22DB0DA6/156907.png'/> </span>
-    <span v-if='gw2.copper'>{{gw2.copper.toLocaleString()}}<img src='https://render.guildwars2.com/file/6CF8F96A3299CFC75D5CC90617C3C70331A1EF0E/156902.png'/> </span>
+    <span v-if="value==0">
+      <span>0<img src='https://render.guildwars2.com/file/6CF8F96A3299CFC75D5CC90617C3C70331A1EF0E/156902.png'/> </span>
+    </span>
+    <span v-else>
+      <span v-if='gw2.gold'>{{gw2.gold.toLocaleString()}}<img src='https://render.guildwars2.com/file/090A980A96D39FD36FBB004903644C6DBEFB1FFB/156904.png'/> </span>
+      <span v-if='gw2.silver'>{{gw2.silver.toLocaleString()}}<img src='https://render.guildwars2.com/file/E5A2197D78ECE4AE0349C8B3710D033D22DB0DA6/156907.png'/> </span>
+      <span v-if='gw2.copper'>{{gw2.copper.toLocaleString()}}<img src='https://render.guildwars2.com/file/6CF8F96A3299CFC75D5CC90617C3C70331A1EF0E/156902.png'/> </span>
+    </span>
   </span>
 </template>
 
